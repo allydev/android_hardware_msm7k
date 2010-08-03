e651d5787b23e8f57a0e5e47951a626a4a2b05a0
Jessica Gonzalez <jgaona@codeaurora.org>
2010-08-03 16:36:45 -0700

    hardware/msm7k/libcopybit:image flip error in copybit fixed added padding variable
    Change-Id: I6656cdee861680d9b3b8a7ef39304ffa31fff3b0

--- libcopybit/copybit.cpp
+++ libcopybit/copybit.cpp
@@ -190,7 +190,7 @@ static void set_rects(struct copybit_context_t *dev,
     MULDIV(&e->src_rect.y, &e->src_rect.h, src->b - src->t, H);
     if (dev->mFlags & COPYBIT_TRANSFORM_FLIP_V) {
         if (dev->mFlags & COPYBIT_TRANSFORM_ROT_90) {
-            e->src_rect.x = e->src.width - (e->src_rect.x + e->src_rect.w);
+            e->src_rect.x = e->src.width - (e->src_rect.x + e->src_rect.w) - padding;
         }else{
             e->src_rect.y = e->src.height - (e->src_rect.y + e->src_rect.h);
         }
@@ -200,7 +200,7 @@ static void set_rects(struct copybit_context_t *dev,
         if (dev->mFlags & COPYBIT_TRANSFORM_ROT_90) {
             e->src_rect.y = e->src.height - (e->src_rect.y + e->src_rect.h);
         }else{
-            e->src_rect.x = e->src.width - (e->src_rect.x + e->src_rect.w);
+            e->src_rect.x = e->src.width - (e->src_rect.x + e->src_rect.w) - padding;
         }
     }
 }
